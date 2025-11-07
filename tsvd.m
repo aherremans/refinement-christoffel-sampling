01c50d72d51a9c6107840e9f5e99668a19ff2cfe
Astrid Herremans <astrid.herremans@kuleuven.be>
2025-11-07 17:44:13 +0100

    cleanup

--- tsvd.m
+++
@@ -1,9 +0,0 @@
-% Compute A\b using the truncated SVD with absolute tolerance atol.
-function [x,r] = tsvd(A, b, atol)
-    [U,S,V] = svd(A,0);
-    s = diag(S);
-    r = sum(s > atol);
-    S_pinv = zeros(size(S))';
-    S_pinv(1:r,1:r) = diag(s(1:r).^(-1));
-    x = V*(S_pinv*(U'*b));
-end
